0f7914436fbb0f0f3bd8f2e5aa99c2738bca5a1f
Necrobeh <mickael.blanchet5555@gmail.com>
2014-02-06 14:02:11 +0100

    Ajout de pages
    Des pages quoi!

---
+++ nous.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Qui sommes-nous?</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include("header.html"); ?>
+<p>Oui, qui?</p>
+<?php include("footer.html"); ?>
+</body>
+</html>
---
+++ projets.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Projets</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php 
+	include("header.html"); 
+	$bdd = new PDO('mysql:host=localhost;dbname=2ano', 'root', '');
+	
+	include("footer.html"); 
+?>
+</body>
+</html>
